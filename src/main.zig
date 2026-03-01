bd080968c86dd06667d19ce62ed75d71b771e251
foxdefox-wq <bluefoxbrownbear@gmail.com>
2026-03-01 11:00:43 -0800

    e

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,27 @@
 const std = @import("std");
-const r = @import("raylib");
+const rl = @import("raylib");
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    rl.initWindow(800, 600, "Christmas");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        update(rl.getFrameTime());
+
+        rl.clearBackground(.white);
+    }
 }
+
+pub fn update(dt: i32) void {
+    physics(dt);
+    render();
+}
+
+pub fn physics(dt: i32) void {}
+
+pub fn render() void {}
